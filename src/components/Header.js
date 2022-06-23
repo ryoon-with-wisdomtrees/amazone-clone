9e481b9915bd21566d5f9940afa0e36b75b4dfe9
SphereSRYN <sphere.sryn@gmail.com>
2022-06-23 15:13:14 +0900

    the homepage part

---
+++ src/components/Header.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "../styles/Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+function Header() {
+  return (
+    <div className="header">
+      <img
+        src="https://pngimg.com/uploads/amazone/amazone_PNG11.png"
+        className="header__logo"
+        alt=""
+      />
+      <div className="header__search">
+        <input type="text" className="header__searchInput" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+      <div className="header__nav">
+        <div className="header__option">
+          {/** hello*/}
+          <span className="header__optionLine1">Hello Guest</span>
+          <span className="header__optionLine2">Sign In</span>
+        </div>
+        <div className="header__option">
+          {/** returns*/}
+          <span className="header__optionLine1">Returns</span>
+          <span className="header__optionLine2">& Orders</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLine1">Your</span>
+          <span className="header__optionLine2">Prime</span>
+        </div>
+        <div className="header__optionsBasket">
+          <ShoppingBasketIcon />
+          <span className="header__optionLine2 header__basketCount">0</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD0O5GQwcAhUZU9O0Sotq3G3RAdL-lmUks",
+  authDomain: "e-clone-7a86c.firebaseapp.com",
+  projectId: "e-clone-7a86c",
+  storageBucket: "e-clone-7a86c.appspot.com",
+  messagingSenderId: "235241062950",
+  appId: "1:235241062950:web:b51992e1e8a2f08c5d9ce7",
+  measurementId: "G-SCW0BHYT0V",
+};
