96aafb7e14a99376e424be29fa6888636c5d7997
SphereSRYN <sphere.sryn@gmail.com>
2022-06-25 14:41:39 +0900

    3. the checkout page start

--- src/App.js
+++ src/App.js
@@ -2,15 +2,26 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import { Router } from "react-router-dom";
+import { Route } from "react-router";
+import Swit
+import { Switch } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      {/** 필요한 대분류 컴포넌트들 적어놓고 만들고 나면 remove out */}
-      <Header />
-      <Home />
-      {/** Body */}
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <h1> Header desue</h1>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
